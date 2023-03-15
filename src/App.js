23cf827d549d0508cb4a602633d650ed5716789f
nyan92213 <ivanbezrukov92213@gmail.com>
2023-03-15 14:00:01 +0300

    События и this

--- src/App.js
+++ src/App.js
@@ -1,20 +1,55 @@
+import { Component } from 'react';
 
 import './App.css';
 
-function WhoAmI({name, surname, link}) { 
-  return (
-    <div>
-      <h1>My name is {name()}, surname - {surname}</h1>
-      <a href={link}>My profile</a>
-    </div>
-  )
+class WhoAmI extends Component { 
+  constructor(props) {
+    super(props)
+    this.state = {
+      years: 27,
+      text: '+++',
+      position: ''
+    }
+    // this.nextYear = this.nextYear.bind(this) // привязывает метод к конкретному экземпляру класса
+  }
+
+  nextYear() {
+    this.setState(state => ({
+        years: state.years + 1
+      }))
+  }
+
+  commitInputChanges = (e) => {
+    this.setState({
+      position: e.target.value
+    })
+  }
+
+  render()  {
+    const {name, surname, link} = this.props
+    const {position, years} = this.state
+
+    return (
+      <div>
+        <button onClick={() => this.nextYear()}>{this.state.text}</button>
+        <h1>My name is {name}, surname - {surname}, 
+        age - {years}, 
+        position - {position}</h1>
+        <a href={link}>My profile</a>
+        <form>
+          <span>Введите должность</span>
+          <input type="text" onChange={this.commitInputChanges} />
+        </form>
+      </div>
+    )
+  }
 }
 
 function App() {
   return (
     <div className="App">
-      <WhoAmI name={() => {return 'Jhon'}} surname="Smith" link="vk.com"/>
-      <WhoAmI name={() => {return 'Alex'}} surname="Shepard" link="facebook.com"/>
+      <WhoAmI name='Jhon' surname="Smith" link="vk.com"/>
+      <WhoAmI name='Alex' surname="Shepard" link="facebook.com"/>
     </div>
   );
 }
