113f5f685431c6db78c7a8b895377a4617aa76c2
nyan92213 <ivanbezrukov92213@gmail.com>
2023-03-24 11:03:18 +0300

    added useRef

--- src/App.js
+++ src/App.js
@@ -1,70 +1,60 @@
+import { useRef, useState, useEffect } from 'react';
 import React, {Component} from 'react';
-import ReactDOM from 'react-dom';
 import {Container} from 'react-bootstrap';
 import './App.css';
 
-class Form extends Component {
+const Form = () => {
 
-    state = {
-        advOpen: false
-    }
+    const [text, setText] = useState('')
 
-    componentDidMount() {
-        setTimeout(this.handleClick, 1000)
-        console.log('piu')
-    }
+    const myRef = useRef(1)
 
-    handleClick = () => {
-        this.setState(({advOpen}) => ({
-            advOpen: !advOpen
-        }))
+
+    // const focusFirstTI = () => {
+    //     myRef.current.focus() // если callback - не используется current
+    // }
+
+    useEffect(() => {
+        myRef.current = text
+    })
+ 
+    return (
+        <Container>
+            <form className="w-50 border mt-5 p-3 m-auto">
+                <div className="mb-3">
+                    <label htmlFor="exampleFormControlInput1" className="form-label">Email address</label>
+                    {/* <TextInput ref={this.myRef}/> */}
+                    <input 
+                    onChange={(e) => setText(e.target.value)}
+                    type="email" 
+                    className="form-control" 
+                    id="exampleFormControlInput1" 
+                    placeholder="name@example.com"/>
+                </div>
+                <div className="mb-3">
+                    <label htmlFor="exampleFormControlTextarea1" className="form-label">Example textarea</label>
+                    <textarea value={myRef.current} className="form-control" id="exampleFormControlTextarea1" rows="3"></textarea>
+                </div>
+            </form>
+        </Container>
+    )
+}
+ 
+class TextInput extends Component {
+
+    doSmth = () => {
+        console.log('Do smth')
     }
 
     render() {
         return (
-            <Container>
-                <form onClick={this.handleClick} className="w-50 border mt-5 p-3 m-auto" 
-                style={{'overflow': 'hidden', 
-                        'position': 'relative'}}>
-                    <div className="mb-3">
-                        <label htmlFor="exampleFormControlInput1" className="form-label">Email address</label>
-                        <input  type="email" className="form-control" id="exampleFormControlInput1" placeholder="name@example.com"/>
-                    </div>
-                    <div className="mb-3">
-                        <label htmlFor="exampleFormControlTextarea1" className="form-label">Example textarea</label>
-                        <textarea className="form-control" id="exampleFormControlTextarea1" rows="3"></textarea>
-                    </div>
-                    {
-                        this.state.advOpen ? 
-                            <Portal>
-                                <Msg/>
-                            </Portal> : null
-                    }
-                </form>
-            </Container>
+            <input 
+            type="email" 
+            className="form-control" 
+            id="exampleFormControlInput1" 
+            placeholder="name@example.com"/>
         )
     }
-}
-
-const Portal = (props) => {
-    const node = document.createElement('div')
-    document.body.appendChild(node)
-
-    return ReactDOM.createPortal(props.children, node)
-}
-
-const Msg = () => {
-    return (
-        <div 
-            style={{'width': '500px', 
-                'height': '150px', 
-                'backgroundColor': 'red', 
-                'position': 'absolute', 
-                'right': '0', 
-                'bottom': '0'}}>
-            Hello
-        </div>
-    )
 }
 
 function App() {
